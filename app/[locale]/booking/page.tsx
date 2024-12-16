608072d99a2cd0c6f3c9152802c282a04925c7f4
Romain Piveteau <piveteauit@gmail.com>
2024-12-16 18:01:36 +0100

    fix bad nuit resa styel

--- app/[locale]/booking/page.tsx
+++ app/[locale]/booking/page.tsx
@@ -165,7 +165,7 @@ export default function ReservationPage({ params: { locale } }: any) {
             onLoad={() => handleIframeParams()}
             frameBorder="0"
             width="100%"
-            height={typeof window !== "undefined" && window.innerWidth >= 1024 ? "1400": "2000"}
+            height={typeof window !== "undefined" && window.innerWidth >= 1024 ? "1400": "2200"}
             title="Reservation Form"
           />
         </motion.div>
