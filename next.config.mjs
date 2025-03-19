b9d551de1481cd981e16a32e132f87982041a108
Romain Piveteau <piveteauit@gmail.com>
2025-03-19 05:48:51 +0100

    rewrite setgn

--- next.config.mjs
+++ next.config.mjs
@@ -30,22 +30,27 @@ const nextConfig = {
       {
         source: "/:locale/dashboard/:path*",
         destination: `${process.env.AZBOOKED_APP_URL}/:locale/dashboard/:path*`,
+        basePath: false,
       },
       {
         source: "/:locale/auth/:path*",
         destination: `${process.env.AZBOOKED_APP_URL}/:locale/auth/:path*`,
+        basePath: false,
       },
       {
         source: "/:locale/dashboard",
         destination: `${process.env.AZBOOKED_APP_URL}/:locale/dashboard`,
+        basePath: false,
       },
       {
         source: "/_next/:path*",
         destination: `${process.env.AZBOOKED_APP_URL}/_next/:path*`,
+        basePath: false,
       },
       {
         source: "/__nextjs_font/:path*",
         destination: `${process.env.AZBOOKED_APP_URL}/__nextjs_font/:path*`,
+        basePath: false,
       },
     ];
   },
